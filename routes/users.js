fe81558307a8ee2b3a6660c6e1f857549ba80bb6
Paulo Bichara <contato@paulobichara.org>
2022-03-14 11:24:17 -0300

    adding prisma, express validator and end points

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const validateUser = require("../middlewares/validators/user");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var router = express.Router();
+const prisma = new PrismaClient();
+
+router.get("/", async (_, res) => {
+  return res.json(await prisma.user.findMany());
+});
+
+router.post("/", ...validateUser, async (req, res) => {
+  const newUser = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+    },
+  });
+  res.json(newUser);
+});
+
+router.get("/:userId/posts/", async (_, res) => {
+  return res.json(await prisma.post.findMany());
+});
+
+router.post("/:userId/posts/", async (req, res) => {
+  const newPost = await prisma.post.create({
+    data: {
+      title: req.body.title,
+      content: req.body.content,
+      published: req.body.published,
+      authorId: Number.parseInt(req.params.userId, 10),
+    },
+  });
+  res.json(newPost);
 });
 
 module.exports = router;
