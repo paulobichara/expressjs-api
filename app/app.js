5720d9a7aa97d481a6a613b626019e441ad7d1c2
Paulo Bichara <contato@paulobichara.org>
2022-03-15 15:41:30 -0300

    removing index route

--- app/app.js
+++ app/app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -15,7 +14,6 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
